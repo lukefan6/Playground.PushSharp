90b262ba747e2ceb3eb1968245d18a30ced78865
lukefan6 <wakefan6@gmail.com>
2015-03-26 15:38:56 +0800

    Setting up UI

--- Playground.PushSharp/Playground.PushSharp.WindowsForm/Main.Designer.cs
+++ Playground.PushSharp/Playground.PushSharp.WindowsForm/Main.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(13, 61);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(228, 23);
+            this.messageTextBox.TabIndex = 0;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(247, 61);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 1;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(331, 349);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Main";
+            this.Text = "PushSharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button submitButton;
     }
 }
 
