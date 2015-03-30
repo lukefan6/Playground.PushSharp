0c00279768e3a8651aa1d97601de676ec5f316e1
lukefan6 <wakefan6@gmail.com>
2015-03-30 17:24:45 +0800

    Rename UI components

--- Playground.PushSharp/Playground.PushSharp.WindowsForm/Main.Designer.cs
+++ Playground.PushSharp/Playground.PushSharp.WindowsForm/Main.Designer.cs
@@ -28,137 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.appleCertificate = new System.Windows.Forms.Label();
+            this.appleCertificateLocation = new System.Windows.Forms.Label();
+            this.tokenLabel = new System.Windows.Forms.Label();
+            this.ChooseAppleCertificate = new System.Windows.Forms.Button();
+            this.appleTokenBox = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.button2 = new System.Windows.Forms.Button();
+            this.badgeNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.alertBodyTextBox = new System.Windows.Forms.TextBox();
+            this.badgeLabel = new System.Windows.Forms.Label();
+            this.labelAlertBody = new System.Windows.Forms.Label();
+            this.pushButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.badgeNumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // appleCertificate
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.appleCertificate.AutoSize = true;
+            this.appleCertificate.Location = new System.Drawing.Point(24, 40);
+            this.appleCertificate.Name = "appleCertificate";
+            this.appleCertificate.Size = new System.Drawing.Size(105, 16);
+            this.appleCertificate.TabIndex = 0;
+            this.appleCertificate.Text = "Apple Certificate:";
             // 
-            // label2
+            // appleCertificateLocation
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(74, 40);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.appleCertificateLocation.AutoSize = true;
+            this.appleCertificateLocation.Location = new System.Drawing.Point(132, 40);
+            this.appleCertificateLocation.Name = "appleCertificateLocation";
+            this.appleCertificateLocation.Size = new System.Drawing.Size(51, 16);
+            this.appleCertificateLocation.TabIndex = 1;
+            this.appleCertificateLocation.Text = "Not Set";
             // 
-            // label3
+            // tokenLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 76);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.tokenLabel.AutoSize = true;
+            this.tokenLabel.Location = new System.Drawing.Point(24, 76);
+            this.tokenLabel.Name = "tokenLabel";
+            this.tokenLabel.Size = new System.Drawing.Size(49, 16);
+            this.tokenLabel.TabIndex = 2;
+            this.tokenLabel.Text = "Token: ";
             // 
-            // button1
+            // ChooseAppleCertificate
             // 
-            this.button1.Location = new System.Drawing.Point(244, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.ChooseAppleCertificate.Location = new System.Drawing.Point(244, 37);
+            this.ChooseAppleCertificate.Name = "ChooseAppleCertificate";
+            this.ChooseAppleCertificate.Size = new System.Drawing.Size(75, 23);
+            this.ChooseAppleCertificate.TabIndex = 3;
+            this.ChooseAppleCertificate.Text = "Choose...";
+            this.ChooseAppleCertificate.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // appleTokenBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(77, 73);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(242, 23);
-            this.textBox1.TabIndex = 4;
+            this.appleTokenBox.Location = new System.Drawing.Point(77, 73);
+            this.appleTokenBox.Name = "appleTokenBox";
+            this.appleTokenBox.Size = new System.Drawing.Size(242, 23);
+            this.appleTokenBox.TabIndex = 4;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.numericUpDown1);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.badgeNumericUpDown);
+            this.groupBox1.Controls.Add(this.alertBodyTextBox);
+            this.groupBox1.Controls.Add(this.badgeLabel);
+            this.groupBox1.Controls.Add(this.labelAlertBody);
             this.groupBox1.Location = new System.Drawing.Point(27, 117);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(292, 100);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
             // 
-            // textBox2
+            // badgeNumericUpDown
             // 
-            this.textBox2.Location = new System.Drawing.Point(56, 27);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(230, 23);
-            this.textBox2.TabIndex = 2;
+            this.badgeNumericUpDown.Location = new System.Drawing.Point(78, 67);
+            this.badgeNumericUpDown.Name = "badgeNumericUpDown";
+            this.badgeNumericUpDown.Size = new System.Drawing.Size(120, 23);
+            this.badgeNumericUpDown.TabIndex = 3;
             // 
-            // label5
+            // alertBodyTextBox
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 69);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(43, 16);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "label5";
+            this.alertBodyTextBox.Location = new System.Drawing.Point(78, 31);
+            this.alertBodyTextBox.Name = "alertBodyTextBox";
+            this.alertBodyTextBox.Size = new System.Drawing.Size(208, 23);
+            this.alertBodyTextBox.TabIndex = 2;
             // 
-            // label4
+            // badgeLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 34);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 16);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "label4";
+            this.badgeLabel.AutoSize = true;
+            this.badgeLabel.Location = new System.Drawing.Point(6, 69);
+            this.badgeLabel.Name = "badgeLabel";
+            this.badgeLabel.Size = new System.Drawing.Size(48, 16);
+            this.badgeLabel.TabIndex = 1;
+            this.badgeLabel.Text = "Badge:";
             // 
-            // numericUpDown1
+            // labelAlertBody
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(55, 67);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
-            this.numericUpDown1.TabIndex = 3;
+            this.labelAlertBody.AutoSize = true;
+            this.labelAlertBody.Location = new System.Drawing.Point(6, 34);
+            this.labelAlertBody.Name = "labelAlertBody";
+            this.labelAlertBody.Size = new System.Drawing.Size(69, 16);
+            this.labelAlertBody.TabIndex = 0;
+            this.labelAlertBody.Text = "Alert Body:";
             // 
-            // button2
+            // pushButton
             // 
-            this.button2.Location = new System.Drawing.Point(244, 265);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.pushButton.Location = new System.Drawing.Point(244, 265);
+            this.pushButton.Name = "pushButton";
+            this.pushButton.Size = new System.Drawing.Size(75, 23);
+            this.pushButton.TabIndex = 6;
+            this.pushButton.Text = "Push";
+            this.pushButton.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(331, 349);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pushButton);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.appleTokenBox);
+            this.Controls.Add(this.ChooseAppleCertificate);
+            this.Controls.Add(this.tokenLabel);
+            this.Controls.Add(this.appleCertificateLocation);
+            this.Controls.Add(this.appleCertificate);
             this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Main";
             this.Text = "PushSharp";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.badgeNumericUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,17 +165,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label appleCertificate;
+        private System.Windows.Forms.Label appleCertificateLocation;
+        private System.Windows.Forms.Label tokenLabel;
+        private System.Windows.Forms.Button ChooseAppleCertificate;
+        private System.Windows.Forms.TextBox appleTokenBox;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox alertBodyTextBox;
+        private System.Windows.Forms.Label badgeLabel;
+        private System.Windows.Forms.Label labelAlertBody;
+        private System.Windows.Forms.NumericUpDown badgeNumericUpDown;
+        private System.Windows.Forms.Button pushButton;
 
     }
 }
